912fa9746a6c1c45d9a875135bc79ebc2eb913f6
Dr.Strange <nazaroff.serezha2014@gmail.com>
2021-02-02 14:14:58 +0300

    Добавил в Students окно добавления новго студента

---
+++ Class_folder/Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student
+{
+    class Student
+    {
+        private String Name;
+        private List<int> Marks;
+        public string Name1 { get => Name; set => Name = value; }
+        public List<int> Marks1 { get => Marks; set => Marks = value; }
+        
+        public int _Sred()
+        {
+            int Sum = 0;
+            foreach (int i in Marks1)
+            {
+                Sum = i + Sum;
+            }
+            Sum = Sum / Marks1.Count();
+            return Sum;
+        }
+    }
+}
---
+++ Strudent/Student/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Student;
+
+namespace Student
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        List<student> Students = new List<student>();
+        student student1 = new student();
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 Add_window = new Window1();
+            student1.Name1 = Add_window.name;
+            Students.Add(student1);
+        }
+    }
+}
